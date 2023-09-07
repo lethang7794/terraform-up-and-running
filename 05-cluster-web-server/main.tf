b8bc55030aa82acc35f8d631b11f1bf476cde0c9
Le Quang Thang <lethang7794@gmail.com>
2023-09-07 15:17:02 +0700

    Add comment and explanation for deploy a cluster of web servers

--- 05-cluster-web-server/main.tf
+++ 05-cluster-web-server/main.tf
@@ -19,9 +19,8 @@ variable "server_port" {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# AUTO SCALING GROUP
+# AUTO SCALING GROUP (ACG)
 # ---------------------------------------------------------------------------------------------------------------------
-
 resource "aws_launch_configuration" "my_launch_config" {
   image_id        = "ami-0464f90f5928bccb8"
   instance_type   = "t2.micro"
@@ -55,7 +54,7 @@ resource "aws_launch_configuration" "my_launch_config" {
               fi
               EOF
 
-  # Required when using a launch configuration with an auto scaling group.
+  # Required when using a launch configuration with an ASG.
   lifecycle {
     create_before_destroy = true
   }
@@ -63,10 +62,12 @@ resource "aws_launch_configuration" "my_launch_config" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.my_launch_config.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
 
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
+  vpc_zone_identifier = data.aws_subnets.default.ids # Use all the subnets in our Default VPC (by using the aws_subnets data source)
+  # ⚠️ In production, the Auto Scaling Group should run in a private subnet
+
+  target_group_arns = [aws_lb_target_group.asg.arn] # Attach our Auto Scaling Group with load balancer target group. AWS will automatically add and remove instances from the target group over its life cycle
+  health_check_type = "ELB" # Use the target group’s health check 
 
   min_size = 2
   max_size = 5
@@ -84,10 +85,11 @@ resource "aws_autoscaling_group" "example" {
 resource "aws_lb" "example" {
   name               = "terraform-asg-example"
   load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids # Use all the subnets in our Default VPC (by using the aws_subnets data source). ⚠️ In production, we should create a new VPC 
+  security_groups    = [aws_security_group.alb.id]  # Use the security group that allow HTTP rueqest on port 80
 }
 
+# A LB Listerer that listens on port 80 and protocol HTTP
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
   port              = 80
@@ -105,6 +107,7 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+# A LB Listerer Rule that sends requests that match any path to the target group that contains our Auto Scaling Group.
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
   priority     = 100
@@ -114,13 +117,14 @@ resource "aws_lb_listener_rule" "asg" {
       values = ["*"]
     }
   }
-  
+
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.asg.arn
   }
 }
 
+# A LB Target Group that performs health check our Instances by periodically sending an HTTP request to each Instance
 resource "aws_lb_target_group" "asg" {
   name     = "terraform-asg-example"
   port     = var.server_port
@@ -138,15 +142,21 @@ resource "aws_lb_target_group" "asg" {
   }
 }
 
+# We don't need to create aws_lb_target_group_attachment to provide a static list of EC2 instance to Target Group
+# With an Auto Scaling Group, Instances can launch or terminate at any time, so a static list won’t work
+# The Auto Scaling Group has a first class integration with the Load Balancer, we just need to point the ASC to the Target Group
+
 # ---------------------------------------------------------------------------------------------------------------------
 # SECURITY GROUP
 # ---------------------------------------------------------------------------------------------------------------------
 resource "random_uuid" "my_uuid" {
 }
 
+# A Security Group that allows inbound request to EC2 instance port
 resource "aws_security_group" "ec2_instance" {
   name = "ec2-sg-${random_uuid.my_uuid.result}"
 
+ # Allow inbound HTTP requests on the server port
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
@@ -154,13 +164,7 @@ resource "aws_security_group" "ec2_instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # ⚠️ DO NOT DO THIS IN PRODUCTION
-  }
-
+  # Allow all outbound requests: so the EC2 instance can response to incoming request
   egress {
     from_port   = 0
     to_port     = 0
@@ -169,10 +173,12 @@ resource "aws_security_group" "ec2_instance" {
   }
 }
 
+# By default, all AWS resources, including ALBs, don’t allow any incoming or outgoing traffic
+# We need to create create a security group to allow incoming HTTP request on port 80
 resource "aws_security_group" "alb" {
   name = "alb-sg-${random_uuid.my_uuid.result}"
 
-  # Allow inbound HTTP requests
+  # Allow inbound HTTP requests on port 80
   ingress {
     from_port   = 80
     to_port     = 80
@@ -180,7 +186,7 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound requests
+  # Allow all outbound requests: so load balancer can perform health checks
   egress {
     from_port   = 0
     to_port     = 0
